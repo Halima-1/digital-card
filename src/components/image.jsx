c51c9b85f575463274bad97dad28197c4cd5dd46
dahunsi halima <dahunsiolajumoke18@gmail.com>
2025-08-28 17:32:00 +0100

    image added

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -3,7 +3,7 @@ const Image =()=>{
     return(
         <>
     <div className='image'>
-        <img src="https://www.freepik.com/premium-ai-image/hoodie-mockup-designs_277583083.htm#fromView=search&page=1&position=18&uuid=907b5eff-f729-4a3c-9a18-79e0ad6be457&query=Cute+girl+avatar+head+covered" alt="card image" />
+        <img src="https://www.freepik.com/premium-ai-image/png-3d-cartoon-realistic-bangladeshi-middle-aged-woman-portrait-adult-white-background_413548320.htm#fromView=search&page=1&position=34&uuid=a78d5ad9-6f69-4754-b724-b99727909169&query=muslim+girl+avatar" alt="card image" />
     </div>
     </>
     )
