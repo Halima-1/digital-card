673aef9d795c9558c50ad9bdbb58dbf30004817d
dahunsi halima <dahunsiolajumoke18@gmail.com>
2025-08-28 17:24:20 +0100

    card updated

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -3,7 +3,7 @@ const Image =()=>{
     return(
         <>
     <div className='image'>
-        <img src="../src/assets/IMG_3348.JPG" alt="card image" />
+        <img src="https://www.freepik.com/premium-ai-image/hoodie-mockup-designs_277583083.htm#fromView=search&page=1&position=18&uuid=907b5eff-f729-4a3c-9a18-79e0ad6be457&query=Cute+girl+avatar+head+covered" alt="card image" />
     </div>
     </>
     )
